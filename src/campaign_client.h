a790e518176111002f548c869b1c2ea69bad8b59
Pithlit <Pithlit@mail.de>
2022-08-13 16:44:48 +0200

    Campaign Client
    add campaign client to server communication:
    
    * http communication with an external campaign server via REST/json
    * send notifications
    * get scenario list
    * get scenario info
    * get available ships
    * get available settings
    * proxySpawn

---
+++ src/campaign_client.h
@@ -0,0 +1,48 @@
+#ifndef CAMPAIGN_CLIENT_H 
+#define CAMPAIGN_CLIENT_H 
+
+#include <stdint.h>
+#include <thread>
+#include <future>
+#include <map>
+#include <vector>
+#include "io/network/udpSocket.h"
+#include "io/network/tcpSocket.h"
+#include "io/network/streamSocket.h"
+#include "io/network/tcpListener.h"
+#include "Updatable.h"
+#include "stringImproved.h"
+#include <io/json.h>
+#include <io/http/request.h>
+
+class CampaignClient;
+
+extern P<CampaignClient> campaign_client;
+
+class CampaignClient: public virtual PObject
+{
+    string campaign_server_hostname;
+    int campaign_server_port;
+
+public:
+    CampaignClient(string hostname, int port);
+    bool isOnline();
+    void notifyCampaignServer(string event, nlohmann::json scenario_info);
+    std::vector<string> getScenarios();
+    std::map<string, string> getScenarioInfo(string name);
+    std::map<string, std::vector<string> > getScenarioSettings(string name);
+    std::vector<string> getShips();
+    string getCampaignServerURL() { return campaign_server_hostname; }
+    void spawnShipOnProxy(string server_ip, string ship_name, string ship_template, string ship_password);
+private:
+    void httpGetNoResponse(string path);
+    void httpPostNoResponse(string path, string body);
+    std::future<string> httpGet(string path);
+    std::future<string> httpPost(string path, string body);
+    nlohmann::json httpGetJson(string path);
+
+    string httpRequest(const string& path, const string& body="", bool post = false);
+    const string urlencode(const string&);
+};
+
+#endif//MULTIPLAYER_SERVER_H
